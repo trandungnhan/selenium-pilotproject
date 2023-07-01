f49b73f7de84bf94c2421ef9b159b1df6b95956b
Tran Dung Nhan <trandungnhan89@gmail.com>
2023-07-01 20:37:19 +0700

    init project

---
+++ src/test/java/magento/magentoTest.java
@@ -0,0 +1,33 @@
+package magento;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class magentoTest {
+
+    @Test
+    void verifyWelcomeMegs() {
+        By welcomeMegElem = By.xpath("//div[@class='panel header']/*//span[@class='not-logged-in']");
+
+        int TIME_OUT_IN_SECONDS = 30;
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait;
+        wait= new WebDriverWait(driver, Duration.ofSeconds(TIME_OUT_IN_SECONDS));
+
+        driver.get("https://magento.softwaretestingboard.com/");
+
+        String welcomeMeg = wait.until(ExpectedConditions.visibilityOfElementLocated(welcomeMegElem)).getText();
+
+        Assert.assertEquals(welcomeMeg,"Default welcome msg!");
+
+        driver.quit();
+
+    }
+}
---
+++ src/test/java/theinternet/LoginPageTest.java
@@ -0,0 +1,35 @@
+package theinternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+
+    @Test
+    void LoginSuccess(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        By usernameTextBox = By.id("username");
+        By passwordTextBox = By.id("password");
+        By submitButton = By.cssSelector("button[type='submit']");
+        By messageElem = By.cssSelector("h4[class='subheader']");
+
+
+        driver.findElement(usernameTextBox).sendKeys("tomsmith");
+        driver.findElement(passwordTextBox).sendKeys("SuperSecretPassword!");
+        driver.findElement(submitButton).click();
+
+        String successfulMeg = driver.findElement(messageElem).getText();
+        Assert.assertEquals(successfulMeg,"Welcome to the Secure Area. When you are done click logout below.");
+
+
+
+
+    }
+
+
+}
