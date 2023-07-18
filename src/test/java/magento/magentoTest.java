0f08d6e8c9ff2f44d0668b0d9d2c5b8ac595713d
Tran Dung Nhan <trandungnhan89@gmail.com>
2023-07-18 21:49:03 +0700

    add test cases verifyInformationOfProduct

--- src/test/java/magento/magentoTest.java
+++ src/test/java/magento/magentoTest.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class magentoTest {
 
@@ -118,12 +123,98 @@ public class magentoTest {
         driver.quit();
     }
 
+    @Test
+    void verifyInformationOfProduct() throws InterruptedException {
+        By searchTextBox = By.cssSelector("input[id='search']");
+        By notificationElem = By.cssSelector("span[class='base']");
+        By firstProductElem = By.cssSelector("ol[class='products list items product-items'] li:nth-of-type(1)");
+        By secondProductElem = By.cssSelector("ol[class='products list items product-items'] li:nth-of-type(2)");
+
+
+        By firstSizeElem = By.xpath("//ol[@class='products list items product-items']/li[1]//div[@option-label='32']");
+        By secondSizeElem = By.xpath("//ol[@class='products list items product-items']/li[2]//div[@option-label='33']");
+
+        By firstColorElem = By.xpath("//ol[@class='products list items product-items']/li[1]//div[@option-label='Black']");
+        By secondColorElem = By.xpath("//ol[@class='products list items product-items']/li[2]//div[@option-label='Brown']");
+
+
+        By firstAddToCartElem = By.xpath("//ol[@class='products list items product-items']/li[1]//button[@title='Add to Cart']");
+        By secondAddToCartElem = By.xpath("//ol[@class='products list items product-items']/li[2]//button[@title='Add to Cart']");
+
+        By showCartElem = By.xpath("//a[@class='action showcart']");
+        By countNumberItemElem = By.xpath("//span[@class='counter-number'][contains(text(),*)]");
+        By checkoutButton = By.cssSelector("button[id='top-cart-btn-checkout']");
+        By nameOfProduct1 = By.xpath("//ol[@class='minicart-items']//li[1]//strong[@class='product-item-name']");
+        By nameOfProduct2 = By.xpath("//ol[@class='minicart-items']//li[2]//strong[@class='product-item-name']");
+
+        By viewDetailsProduct1 = By.xpath("//ol[@class='minicart-items']//li[1]//span[@class='toggle']");
+        By viewDetailsProduct2 = By.xpath("//ol[@class='minicart-items']//li[2]//span[@class='toggle']");
+        By sizeOfProduct = By.xpath("//div[@class='product options active']//dd[@class='values'][1]");
+        By colorOfProduct = By.xpath("//div[@class='product options active']//dd[@class='values'][2]");
+
+        //WebDriver driver = new ChromeDriver();
+
+        WebDriver driver;
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+        driver = new ChromeDriver(chromeOptions);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        int TIME_OUT_IN_SECONDS = 50;
+        WebDriverWait wait;
+        wait= new WebDriverWait(driver, Duration.ofSeconds(TIME_OUT_IN_SECONDS));
+
+        driver.get("https://magento.softwaretestingboard.com/");
+
+        driver.manage().window().maximize();
+
+        driver.findElement(searchTextBox).sendKeys("pants" + Keys.ENTER);
+
+        String notification = driver.findElement(notificationElem).getText();
+
+        Assert.assertEquals(notification, "Search results for: 'pants'");
+
+        addProductToCart(driver, wait, js, firstProductElem, firstSizeElem, firstColorElem, firstAddToCartElem );
+
+        String numberOfItems = getTotalItem(driver, wait, js, showCartElem ,countNumberItemElem);
+
+        Assert.assertEquals(numberOfItems,"1");
+
+        addProductToCart(driver, wait, js, secondProductElem, secondSizeElem, secondColorElem, secondAddToCartElem );
+
+        String numberOfItems2 = getTotalItem(driver, wait, js, showCartElem ,countNumberItemElem);
+
+        Assert.assertEquals(numberOfItems2,"2");
+
+        driver.findElement(showCartElem).click();
+
+        driver.findElement(checkoutButton).click();
+
+        driver.findElement(viewDetailsProduct1).click();
+
+        Assert.assertEquals(driver.findElement(nameOfProduct1).getText(),"Caesar Warm-Up Pant");
+        Assert.assertEquals(driver.findElement(sizeOfProduct).getText(),"32");
+        Assert.assertEquals(driver.findElement(colorOfProduct).getText(),"Black");
+
+        driver.findElement(viewDetailsProduct1).click();
+
+        driver.findElement(viewDetailsProduct2).click();
+        Assert.assertEquals(driver.findElement(nameOfProduct2).getText(),"Caesar Warm-Up Pant");
+        Assert.assertEquals(driver.findElement(sizeOfProduct).getText(),"32");
+        Assert.assertEquals(driver.findElement(colorOfProduct).getText(),"Black");
+
+        driver.quit();
+    }
+
     public String getTotalItem(WebDriver driver, WebDriverWait wait,JavascriptExecutor js,By cartButton, By cartValue ) throws InterruptedException {
 
         js.executeScript("arguments[0].scrollIntoView();", driver.findElement(cartButton));
-        Thread.sleep(10000);
+        Thread.sleep(5000);
+
         String numberOfItems = "0";
         String collectedValue = wait.until(ExpectedConditions.visibilityOfElementLocated(cartValue)).getText();
+
         if (!collectedValue.equals("0")){
             numberOfItems = collectedValue;;
             System.out.println("numberOfItems: " + numberOfItems);
